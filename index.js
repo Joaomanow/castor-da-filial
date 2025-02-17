69e79a5a93355b9a29c52ebf1f7242a2086602a3
João <joao.amnoel11@hotmail.com>
2025-02-17 15:03:16 -0300

    commit 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'castor da filial oseugai\n'
+  const msg = 'castor da filial oseugai👍\n'
   res.end(msg);
 });
 
